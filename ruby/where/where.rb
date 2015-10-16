1f12425c31752ea0c7b7fa24393c7993b98740fa
michaelkunc <mjkunc@gmail.com>
2015-10-16 09:12:32 -0500

    initial where solution

--- ruby/where/where.rb
+++ ruby/where/where.rb
@@ -1,5 +1,6 @@
 class Array
   def where(hash = {})
-    self.select { |element| element[hash.keys[0]] == hash.values[0] }
+    self.select { |e| hash.values[0] === e[hash.keys[0]] &&
+    hash.values[-1] === e[hash.keys[-1]] }
   end
 end
