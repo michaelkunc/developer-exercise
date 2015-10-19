11fe9b5f5d79bb2fbd222e386c8ea8ef9a0c2433
michaelkunc <mjkunc@gmail.com>
2015-10-19 15:11:59 -0500

    refactored where

--- ruby/where/where.rb
+++ ruby/where/where.rb
@@ -1,6 +1,6 @@
 class Array
   def where(hash = {})
-    self.select { |e| hash.values[0] === e[hash.keys[0]] &&
-    hash.values[-1] === e[hash.keys[-1]] }
+    raise Argument Error, 'please only use two search terms' if hash.values.length > 2
+    self.select { |e| hash.values[0] === e[hash.keys[0]] && hash.values[1] === e[hash.keys[1]] }
   end
 end
